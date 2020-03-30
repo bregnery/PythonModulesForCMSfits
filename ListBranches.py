c6c6210ee198481f6f384faa4b766d17ce6662d7
Brendan <bjregnery@ucdavis.edu>
2020-03-30 09:17:46 -0500

    List branches for JetHT plots

--- ListBranches.py
+++ ListBranches.py
@@ -1,6 +1,8 @@
 import ROOT as root
 
 #rootfile = root.TFile("Radion_hh_wwww_M3500_MiniAOD.root")
-rootfile = root.TFile("best_HH_results.root")
-tree = rootfile.Get("Events")
+rootfile = root.TFile("BESToutputs.root")
+rootfile.cd("run")
+print rootfile.ls()
+tree = rootfile.Get("run/jetTree")
 tree.Print()
